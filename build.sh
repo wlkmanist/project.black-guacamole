dce513de64515095dc162d727d17869a8a7d9c4b
wlkmanist <t.me/wlkmanist>
2022-10-03 16:36:19 +0300

    build.sh: update
    Signed-off-by: wlkmanist <t.me/wlkmanist>

--- build.sh
+++ build.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 BUILD_START=$(date +"%s")
-
-# echo
-# echo "Clean Build Directory"
-# echo 
-
-# make clean && make mrproper
+VERSION=$2
+PARAM=$1
 
 echo
 echo "Issue Build Commands"
 echo
 
-#CLANG_RELEASE=clang-r450784e
+# CLANG_RELEASE=clang-r450784e
 CLANG_RELEASE=clang-r416183b1
 # CLANG_RELEASE=clang-r383902b
 
@@ -22,7 +18,8 @@ export SUBARCH=arm64
 export CLANG_PATH=/mnt/Data/tools/Android_dev/Clang_Google/linux-x86/$CLANG_RELEASE/bin
 export MKDTIMG_PATH=/mnt/Data/tools/Android_dev/mkdtimg/bin
 export SPLIT_DTB_PATH=/mnt/Data/tools/Android_dev/split-appended-dtb-master
-export PATH=${CLANG_PATH}:${MKDTIMG_PATH}:${SPLIT_DTB_PATH}:${PATH}
+export DEV_PACK_PATH=/home/wlkmanist/Proj/guacamole/dev
+export PATH=${CLANG_PATH}:${MKDTIMG_PATH}:${SPLIT_DTB_PATH}:${DEV_PACK_PATH}:${PATH}
 # export DTC_EXT=/mnt/Data/tools/Android_dev/dtc-1.6.1/dtc
 export DTC_EXT=/home/wlkmanist/Proj/guacamole/project.black-guacamole/out/scripts/dtc/dtc
 # export DTC_EXT=dtc
@@ -31,15 +28,54 @@ export CROSS_COMPILE=/mnt/Data/tools/Android_dev/gcc-linaro-7.5.0-2019.12-x86_64
 export CROSS_COMPILE_ARM32=/mnt/Data/tools/Android_dev/gcc-linaro-7.5.0-2019.12-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export LD_LIBRARY_PATH=/mnt/Data/tools/Android_dev/Clang_Google/linux-x86/$CLANG_RELEASE/bin:$LD_LIBRARY_PATH
 
-echo
-echo "Set DEFCONFIG"
-echo 
-make ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out black_defconfig
+case "$PARAM" in
+  "")
+  ;;
+  "def")
+    echo
+    echo "Set main DEFCONFIG"
+    echo
+    make ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out black_defconfig
+  ;;
+  "dbg")
+    echo
+    echo "Set dbg DEFCONFIG"
+    echo
+    make ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out blackdbg_defconfig
+  ;;
+  "cln")
+    make clean && make mrproper
+  ;;
+  "rdy")
+    if [ -z "$VERSION" ]; then
+	echo "Usage: ./build rdy [version letter]"
+	exit -1
+    else
+	cd ../dev
+	copy_boot.sh $VERSION
+	exit 0
+    fi
+  ;;
+  "--help" | * )
+    echo "Usage: ./build [param (optional)]"
+    echo ""
+    echo "Params:"
+    echo "  [none]: build only"
+    echo "  def   : build with main defconfig"
+    echo "  dbg   : build with debug defconfig"
+    echo "  cln   : make clean and build"
+    echo "  rdy   : build only then export"
+    echo "  --help: display this"
+
+    exit 0
+  ;;
+esac
 
 echo
 echo "Build The Good Stuff"
 echo 
 
+rm -r /home/wlkmanist/Proj/guacamole/project.black-guacamole/out/arch/arm64/boot/*
 make ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out -j4
 
 cd ~/Proj/guacamole/project.black-guacamole/out/arch/arm64/boot/
